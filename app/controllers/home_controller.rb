1f6b1aea9602d92fbef0f373553305a73964f035
Ryan Mac <rymac@rymacs-compy.local>
2010-12-29 21:56:53 -0800

    lots of lil fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @tags = Campaign.tag_counts
     @popular = Campaign.find(:all, :joins => :votes, :limit => 5)
+    @sponsored = Campaign.find :all, :limit => 4
   end
 
 end
