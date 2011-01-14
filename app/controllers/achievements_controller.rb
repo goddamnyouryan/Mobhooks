4eb8220dd9fcee006db6c9ef0b55cdaa4924ab43
Ryan Mac <rymac@rymacs-compy.local>
2011-01-14 02:35:54 -0800

    few more small bug fixes

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,7 +1,7 @@
 class AchievementsController < ApplicationController
   def index
   	@master_badge_user = User.find_by_login("goddamnyouryan")
-  	@master_badge_list = Achievement.find(:all, "user_id = ?", @master_badge_user.id, :order => "kind ASC")
+  	@master_badge_list = Achievement.find(:all, :conditions => ["user_id = ?", @master_badge_user.id], :order => "kind ASC")
   end
 
   def show
