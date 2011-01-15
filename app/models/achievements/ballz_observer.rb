3b808fa0f094756afc26955819406760c2d8bb8c
Ryan Mac <rymac@rymacs-compy.local>
2011-01-15 03:07:30 -0800

    fixed a few other achievement observers

--- app/models/achievements/ballz_observer.rb
+++ app/models/achievements/ballz_observer.rb
@@ -2,6 +2,6 @@ class BallzObserver < ActiveRecord::Observer
   observe :campaign
   
   def after_save(campaign)
-    Ballz.award_achievements_for(user) if campaign.tag_list.include?("sports") || campaign.tag_list.include?("arena") || campaign.tag_list.include?("baseball") || campaign.tag_list.include?("basketball") || campaign.tag_list.include?("football") || campaign.tag_list.include?("soccer") || campaign.tag_list.include?("hockey") || campaign.tag_list.include?("bowling") || campaign.tag_list.include?("tennis") || campaign.tag_list.include?("track") || campaign.tag_list.include?("lacrosse")
+    Ballz.award_achievements_for(campaign.user) if campaign.tag_list.include?("sports") || campaign.tag_list.include?("arena") || campaign.tag_list.include?("baseball") || campaign.tag_list.include?("basketball") || campaign.tag_list.include?("football") || campaign.tag_list.include?("soccer") || campaign.tag_list.include?("hockey") || campaign.tag_list.include?("bowling") || campaign.tag_list.include?("tennis") || campaign.tag_list.include?("track") || campaign.tag_list.include?("lacrosse")
   end
 end
