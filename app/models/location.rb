97ddbba70f24d776b5d63f6b5617e4d85601ed25
Ryan Mac <rymac@rymacs-compy.local>
2011-01-25 00:12:43 -0800

    trying something with search

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,6 +5,7 @@ class Location < ActiveRecord::Base
   attr_accessible :business_id, :address, :city, :state, :zip
   
   belongs_to :business
+  has_many :campaigns, :through => :business
   
   validates_presence_of :address, :city, :state, :zip
   validates_uniqueness_of :address, :case_sensitive => false, :message => "- This Location has already been entered!"
