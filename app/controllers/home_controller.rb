28d7151a39548cc46e328d10f4fbdde2e84c9e99
Ryan Mac <rymac@rymacs-compy.local>
2011-01-31 01:05:59 -0800

    sponsored update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   def index
     @tags = Campaign.tag_counts
     @popular = Campaign.find(:all, :joins => :votes, :limit => 5)
-    @sponsored = Campaign.find :all, :limit => 4
+    @sponsored1 = Campaign.find 1
+    @sponsored2 = Campaign.find 2
+    @sponsored3 = Campaign.find 6
+    @sponsored4 = Campaign.find 13
+    @sponsored = [@sponsored1, @sponsored2, @sponsored3, @sponsored4]
+    @recent = Campaign.find :all, :order => "created_at DESC", :limit => 10
   end
 
 end
