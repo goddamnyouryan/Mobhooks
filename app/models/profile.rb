8e32bd0e6b5fccbc67362c051366ac919e357898
Ryan Mac <rymac@rymacs-compy.local>
2011-02-13 12:18:12 -0800

    added shit brady wanted

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   attr_accessible :user_id, :name, :zip, :sex, :birthday, :website, :quote, :photo
   
-  has_attached_file :photo, :styles => { :thumb => "90x90#" }, 
+  has_attached_file :photo, :styles => { :thumb => "90x90#", :leaderboard => "60x60#", :leaderboard_small => "30x30#" }, 
                             :storage => :s3, 
                             :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
                             :path => ':id/:style'
