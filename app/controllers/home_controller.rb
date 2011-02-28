07d0a6650100169b2e8fa5c1dc24b107d5d146d2
Ryan Mac <rymac@rymacs-compy.local>
2011-02-27 22:29:33 -0800

    added printable coupons link back in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,12 @@ class HomeController < ApplicationController
     @tags = Campaign.tag_counts
     @popular_un_unique = Campaign.find :all, :joins => :votes, :conditions => ["votes.created_at >=?", 1.week.ago], :limit => 5
     @popular = @popular_un_unique.uniq
-    @sponsored1 = Campaign.find 1
-    @sponsored2 = Campaign.find 4
-    @sponsored3 = Campaign.find 6
-    @sponsored4 = Campaign.find 11
-    @sponsored = [@sponsored1, @sponsored2, @sponsored3, @sponsored4]
-    # @sponsored = Campaign.find :all, :limit => 4
+    #@sponsored1 = Campaign.find 1
+    #@sponsored2 = Campaign.find 4
+    #@sponsored3 = Campaign.find 6
+    #@sponsored4 = Campaign.find 11
+    #@sponsored = [@sponsored1, @sponsored2, @sponsored3, @sponsored4]
+    @sponsored = Campaign.find :all, :limit => 4
     @recent = Campaign.find :all, :order => "created_at DESC", :limit => 10
   end
 
