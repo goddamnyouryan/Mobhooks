570e02aefb5d911b58a087785306688a63841a6b
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-12-05 21:35:11 -0800

    added daily deal widget

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,16 @@
 class HomeController < ApplicationController
+  
+  require 'net/http'
+  require 'rexml/document'
+  
   def index
     @tags = Campaign.tag_counts
     @popular_un_unique = Campaign.find :all, :joins => :votes, :conditions => ["votes.created_at >=?", 1.week.ago], :limit => 5
     @popular = @popular_un_unique.uniq
-    @sponsored1 = Campaign.find 232
-    @sponsored2 = Campaign.find 1
-    @sponsored3 = Campaign.find 228
-    @sponsored4 = Campaign.find 227
-    @sponsored = [@sponsored1, @sponsored2, @sponsored3, @sponsored4]
-    #@sponsored = Campaign.find :all, :limit => 4
+    @yup = "http://steals.mobhooks.com/publishers/mobhooks/deal-of-the-day.xml"
+    @xml = Net::HTTP.get_response(URI.parse(@yup))
+    @doc = REXML::Document.new(@xml.body)
+    @read = @doc.elements.to_a
     @recent = Campaign.find :all, :order => "created_at DESC", :limit => 10
   end
   
